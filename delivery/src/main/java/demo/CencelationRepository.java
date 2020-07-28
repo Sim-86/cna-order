ef705cd74539c204c0d6cc2dcb3357c592aeec1a
Sim-86 <shs12kkk@naver.com>
2020-07-28 15:25:21 +0900

    1st commit

---
+++ delivery/src/main/java/demo/CencelationRepository.java
@@ -0,0 +1,8 @@
+package demo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CencelationRepository extends PagingAndSortingRepository<Cencelation, Long>{
+
+
+}
